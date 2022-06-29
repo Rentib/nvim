487135ea8c3c635fe0182d10d642ac91e4805ff1
Rentib <sbitner420@tutanota.com>
2022-06-29 10:59:21 +0200

    removed mapping colision

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -12,16 +12,12 @@ vim.keymap.set("n", "]d",        vim.diagnostic.goto_next)
 -- LSP settings
 local on_attach = function(_, bufnr)
   local opts = { buffer = bufnr }
-  vim.keymap.set("n", "gD",         vim.lsp.buf.declaration, opts)
-  vim.keymap.set("n", "gd",         vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "K",          vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "gi",         vim.lsp.buf.implementation, opts)
-  vim.keymap.set("n", "<C-k>",      vim.lsp.buf.signature_help, opts)
+  vim.keymap.set("n", "gD",        vim.lsp.buf.declaration, opts)
+  vim.keymap.set("n", "gd",        vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "K",         vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "gi",        vim.lsp.buf.implementation, opts)
   vim.keymap.set("n", "<Space>D",  vim.lsp.buf.type_definition, opts)
   vim.keymap.set("n", "<Space>rn", vim.lsp.buf.rename, opts)
-  vim.keymap.set("n", "gr",         vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<Space>ca", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<Space>so", require('telescope.builtin').lsp_document_symbols, opts)
   vim.api.nvim_create_user_command("Format", vim.lsp.buf.formatting, {})
 end
 
