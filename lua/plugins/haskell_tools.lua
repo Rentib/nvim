0235cb9c0494fa1eeb62747cc0c1e79f7cd0218d
Rentib <sbitner420@tutanota.com>
2024-03-06 15:58:23 +0100

    feat(haskell_tools): mappings

--- lua/plugins/haskell_tools.lua
+++ lua/plugins/haskell_tools.lua
@@ -4,15 +4,17 @@ return {
     config = function()
         local ht = require('haskell-tools')
         local bufnr = vim.api.nvim_get_current_buf()
-        local opts = { noremap = true, silent = true, buffer = bufnr, }
+        local opts = { buffer = bufnr, }
 
-        vim.keymap.set('n', '<space>cl', vim.lsp.codelens.run, opts)
-        vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts)
-        vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
-        vim.keymap.set('n', '<leader>rr', ht.repl.toggle, opts)
-        vim.keymap.set('n', '<leader>rf', function()
+        local m = require('keymap')
+
+        m.n('<space>cl',  vim.lsp.codelens.run, opts)
+        m.n('<space>hs',  ht.hoogle.hoogle_signature, opts)
+        m.n('<space>ea',  ht.lsp.buf_eval_all, opts)
+        m.n('<leader>rr', ht.repl.toggle, opts)
+        m.n('<leader>rf', function()
             ht.repl.toggle(vim.api.nvim_buf_get_name(0))
         end, opts)
-        vim.keymap.set('n', '<leader>rq', ht.repl.quit, opts)
+        m.n('<leader>rq', ht.repl.quit, opts)
     end
 }
