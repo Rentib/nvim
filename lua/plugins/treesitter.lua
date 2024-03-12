7cdd79e7007c9dc1f7c5863836220f4f7d973741
Rentib <sbitner420@tutanota.com>
2024-03-12 10:52:06 +0100

    revert(treesitter): remove textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "VeryLazy" },
-    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
     config = function()
         require("nvim-treesitter.configs").setup({
@@ -13,10 +12,17 @@ return {
 
             highlight = {
                 enable = true,
-                disable = { "latex" }, -- vimtex is better at this
+                disable = function(lang, buf)
+                    if lang == "tex" then return true end
+                    local max_filesize = 1000 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
                 additional_vim_regex_highlighting = false,
             },
-
+            -- indent = { enable = true },
             incremental_selection = {
                 enable = false,
                 keymaps = {
@@ -24,28 +30,6 @@ return {
                     node_incremental  = "<Leader>si",
                     scope_incremental = "<Leader>sc",
                     node_decremental  = "<Leader>sd",
-                },
-            },
-
-            indent = { enable = false },
-
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
-                        -- TODO: perhaps add more objects
-                    },
-
-                    selection_modes = {
-                        -- 'v': charwise, 'V': linewise, '<c-v>': blockwise
-                        ['@function.outer'] = 'V',
-                    },
-                    include_surrounding_whitespace = true,
                 },
             },
         })
