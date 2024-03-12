81d7e21f044baf639d5af523c6b603e5cd942538
Rentib <sbitner420@tutanota.com>
2024-03-12 10:36:24 +0100

    refactor(lsp): remove unchanged defaults

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,14 +17,11 @@ local servers = {
     lua_ls = {
         settings = {
             Lua = {
-                diagnostics = {
-                    globals = { "vim" },
-                },
+                diagnostics = { globals = { "vim" }, },
                 workspace = {
                     library = {
                         [vim.fn.expand "$VIMRUNTIME/lua"] = true,
                         [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-                        [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
                         [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
                     },
                     maxPreload = 100000,
@@ -52,17 +49,6 @@ return {
             -- add binaries created by Mason without loading Mason
             local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
             vim.env.PATH = vim.fn.stdpath "data" .. "/mason/bin" .. (is_windows and ";" or ":") .. vim.env.PATH
-
-            vim.diagnostic.config({
-                float = {
-                    focusable = false,
-                    style = "minimal",
-                    border = "rounded",
-                    source = "always",
-                    header = "",
-                    prefix = "",
-                }
-            })
 
             -- local capabilities = vim.lsp.protocol.make_client_capabilities()
             local capabilities = vim.tbl_deep_extend(
