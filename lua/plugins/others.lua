e8254d34afc9c171db300131945407cae6edc4e6
Rentib <sbitner420@tutanota.com>
2024-03-13 23:05:08 +0100

    chore(update):

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -19,8 +19,6 @@ return {
 
     { 'github/copilot.vim', cmd = 'Copilot' },
 
-    { "farmergreg/vim-lastplace", event = "VeryLazy" },
-
     {
         'folke/trouble.nvim',
         keys = { { '<leader>t', mode = { 'n' }, "<cmd>TroubleToggle<cr>", { noremap = true, silent = true }} },
@@ -44,4 +42,81 @@ return {
             enable_tailwind = false,
         },
     },
+
+    -- TODO: see it these plugins are cool
+    -- { 'Wansmer/treesj' },
+    -- { 'mg979/vim-visual-multi' },
+    -- { 'chentoast/marks.nvim' },
+    -- { 'kylechui/nvim-surround', keys = { 'ys', 'ds', 'cs' } },
+    -- {
+    --     "NeogitOrg/neogit",
+    --     cmd = "Neogit",
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim",
+    --         "sindrets/diffview.nvim",
+    --         "nvim-telescope/telescope.nvim",
+    --     },
+    --     config = true
+    -- },
+    -- {
+    --     "uga-rosa/contextment.vim",
+    --     dependencies = "Shougo/context_filetype.vim",
+    --     keys = {
+    --         { "gc",  "<Plug>(contextment)",      mode = { "n", "x", "o" } },
+    --         { "gcc", "<Plug>(contextment-line)", mode = "n" },
+    --     },
+    -- },
+    -- {
+    --     'seandewar/killersheep.nvim',
+    --     lazy = false,
+    --     config = function ()
+    --         require("killersheep").setup {
+    --             gore = true,           -- Enables/disables blood and gore.
+    --             keymaps = {
+    --                 move_left = "h",     -- Keymap to move cannon to the left.
+    --                 move_right = "l",    -- Keymap to move cannon to the right.
+    --                 shoot = "<Space>",   -- Keymap to shoot the cannon.
+    --             },
+    --         }
+    --     end
+    -- },
+    -- {
+    --     'nvimdev/dyninput.nvim',
+    --     event = "VeryLazy",
+    --     config = function()
+    --         local rs = require('dyninput.lang.rust')
+    --         local ms = require('dyninput.lang.misc')
+    --         require('dyninput').setup({
+    --             c = {
+    --                 ['-'] = {
+    --                     { '->', ms.c_struct_pointer },
+    --                     { '_', ms.snake_case },
+    --                 },
+    --             },
+    --             rust = {
+    --                 [';'] = {
+    --                     { '::', rs.double_colon },
+    --                     { ': ', rs.single_colon },
+    --                 },
+    --                 ['='] = { ' => ', rs.fat_arrow },
+    --                 ['-'] = {
+    --                     { ' -> ', rs.thin_arrow },
+    --                     { '_', ms.snake_case },
+    --                 },
+    --                 ['\\'] = { '|!| {}', rs.closure_fn },
+    --             },
+    --         })
+    --     end,
+    --     dependencies = {'nvim-treesitter/nvim-treesitter'}
+    -- },
+    -- {
+    --     "smjonas/live-command.nvim",
+    --     event = "CmdlineEnter",
+    --     opts = {
+    --         commands = {
+    --             Norm = { cmd = "norm" },
+    --         },
+    --     },
+    -- },
+
 }
