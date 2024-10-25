076da4dabbcc4ebc607d4e19c21479c8e6981d5a
Rentib <sbitner420@tutanota.com>
2024-10-26 00:54:02 +0200

    feat: switch from nvim-cmp to blink.cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,54 +1,74 @@
 return {
-    -- "hrsh7th/nvim-cmp",
-    "iguanacucumber/magazine.nvim",
-    name = "nvim-cmp", -- Otherwise highlighting gets messed up
-    event = "InsertEnter",
-    dependencies = {
-        { "hrsh7th/cmp-nvim-lsp" },
-        { "hrsh7th/cmp-nvim-lsp-signature-help" },
-        { "hrsh7th/cmp-buffer" },
-        { "hrsh7th/cmp-path" },
-        { "saadparwaiz1/cmp_luasnip" },
-        -- { "micangl/cmp-vimtex", ft = "tex" },
+    'saghen/blink.cmp',
+    lazy = false,
+    build = 'cargo build --release',
+    opts = {
+        keymap = {
+            show = { '<c-n>', '<c-p>' },
+            hide = { '<c-e>' },
+            accept = {},
+            select_and_accept = { '<c-y>', '<c-space>' },
+            select_prev = { '<c-p>' },
+            select_next = { '<c-n>' },
+
+            show_documentation = {},
+            hide_documentation = {},
+
+            snippet_forward = { '<c-j>' },
+            snippet_backward = { '<c-k>' },
+        },
+
+        accept = {
+            create_undo_point = true,
+            auto_brackets = {
+                enabled = false,
+            },
+        },
+
+        trigger = {
+            completion = {
+                keyword_range = 'prefix',
+                keyword_regex = '[%w_\\-]',
+                exclude_from_prefix_regex = '[\\-]',
+                blocked_trigger_characters = { ' ', '\n', '\t' },
+                show_on_insert_on_trigger_character = true,
+                show_on_insert_blocked_trigger_characters = { "'", '"' },
+                show_in_snippet = false,
+            },
+
+            signature_help = {
+                enabled = true,
+            },
+        },
+
+        fuzzy = {
+            use_frecency = true,
+            use_proximity = true,
+            max_items = 100,
+            sorts = { 'label', 'kind', 'score' },
+        },
+
+        sources = {
+            completion = {
+                enabled_providers = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+        },
+
+        windows = {
+            autocomplete = {
+                auto_show = true,
+                selection = 'auto_insert', -- 'preselect', 'manual', 'auto_insert'
+                draw = 'minimal',          -- 'simple', 'reversed', 'minimal',
+            },
+            signature_help = {
+                border = 'padded',
+            },
+            ghost_text = {
+                enabled = true,
+            },
+        },
+
+        nerd_font_variant = 'normal', -- 'mono', 'normal'
+        blocked_filetypes = {},
     },
-    config = function()
-        local cmp = require("cmp")
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
-            window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-                ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<C-y>"] = cmp.mapping.confirm {
-                    behavior = cmp.ConfirmBehavior.Insert,
-                    select = true,
-                },
-                ["<C-space>"] = cmp.mapping.confirm {
-                    behavior = cmp.ConfirmBehavior.Insert,
-                    select = true,
-                },
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "nvim_lsp_signature_help" },
-                { name = "luasnip" },
-            }, {
-                { name = "buffer", option = { keyword_pattern = [[\k\+]] } },
-                { name = "path" },
-            }),
-            experimental = {
-                ghost_text = true,
-            },
-            formatting = {
-            },
-        })
-    end
 }
