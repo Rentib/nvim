d3679f5728b3f3972c15652c2775484688f525f3
Rentib <sbitner420@tutanota.com>
2024-10-29 21:30:02 +0100

    feat(cmp): update blink configuration

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,21 +1,91 @@
+-- return {
+--     'saghen/blink.cmp',
+--     lazy = false,
+--     build = 'cargo build --release',
+--     opts = {
+--         keymap = {
+--             show = { '<c-n>', '<c-p>' },
+--             hide = { '<c-e>' },
+--             accept = {},
+--             select_and_accept = { '<c-y>', '<c-space>' },
+--             select_prev = { '<c-p>' },
+--             select_next = { '<c-n>' },
+--
+--             show_documentation = {},
+--             hide_documentation = {},
+--
+--             snippet_forward = { '<c-j>' },
+--             snippet_backward = { '<c-k>' },
+--         },
+--
+--         accept = {
+--             create_undo_point = true,
+--             auto_brackets = {
+--                 enabled = false,
+--             },
+--         },
+--
+--         trigger = {
+--             completion = {
+--                 keyword_range = 'prefix',
+--                 keyword_regex = '[%w_\\-]',
+--                 exclude_from_prefix_regex = '[\\-]',
+--                 blocked_trigger_characters = { ' ', '\n', '\t' },
+--                 show_on_insert_on_trigger_character = true,
+--                 show_on_insert_blocked_trigger_characters = { "'", '"' },
+--                 show_in_snippet = false,
+--             },
+--
+--             signature_help = {
+--                 enabled = true,
+--             },
+--         },
+--
+--         fuzzy = {
+--             use_frecency = true,
+--             use_proximity = true,
+--             max_items = 100,
+--             sorts = { 'label', 'kind', 'score' },
+--         },
+--
+--         sources = {
+--             completion = {
+--                 enabled_providers = { 'lsp', 'path', 'snippets', 'buffer' },
+--             },
+--         },
+--
+--         windows = {
+--             autocomplete = {
+--                 auto_show = true,
+--                 selection = 'auto_insert', -- 'preselect', 'manual', 'auto_insert'
+--                 draw = 'minimal',          -- 'simple', 'reversed', 'minimal',
+--             },
+--             signature_help = {
+--                 border = 'padded',
+--             },
+--             ghost_text = {
+--                 enabled = true,
+--             },
+--         },
+--
+--         nerd_font_variant = 'normal', -- 'mono', 'normal'
+--         blocked_filetypes = {},
+--     },
+-- }
+
 return {
     'saghen/blink.cmp',
     lazy = false,
     build = 'cargo build --release',
     opts = {
         keymap = {
-            show = { '<c-n>', '<c-p>' },
-            hide = { '<c-e>' },
-            accept = {},
-            select_and_accept = { '<c-y>', '<c-space>' },
-            select_prev = { '<c-p>' },
-            select_next = { '<c-n>' },
-
-            show_documentation = {},
-            hide_documentation = {},
-
-            snippet_forward = { '<c-j>' },
-            snippet_backward = { '<c-k>' },
+            ["<c-n>"] = { "show", "select_next", "fallback" },
+            ["<c-p>"] = { "show", "select_prev", "fallback" },
+            ["<c-e>"] = { "hide", "fallback" },
+            ["<c-y>"] = { "select_and_accept", "fallback" },
+            ["<c-space>"] = { "select_and_accept", "fallback" },
+            ["<c-j>"] = { "snippet_forward", "fallback" },
+            ["<c-k>"] = { "snippet_backward", "fallback" },
         },
 
         accept = {
@@ -38,6 +108,9 @@ return {
 
             signature_help = {
                 enabled = true,
+                blocked_trigger_characters = {},
+                blocked_retrigger_characters = {},
+                show_on_insert_on_trigger_character = true,
             },
         },
 
@@ -56,16 +129,23 @@ return {
 
         windows = {
             autocomplete = {
+                border = 'none',
                 auto_show = true,
                 selection = 'auto_insert', -- 'preselect', 'manual', 'auto_insert'
                 draw = 'minimal',          -- 'simple', 'reversed', 'minimal',
-            },
-            signature_help = {
-                border = 'padded',
+                cycle = {
+                    from_bottom = true,
+                    from_top = true,
+                },
             },
             ghost_text = {
                 enabled = true,
             },
+        },
+
+        highlight = {
+            ns = vim.api.nvim_create_namespace('blink_cmp'),
+            use_nvim_cmp_as_default = false,
         },
 
         nerd_font_variant = 'normal', -- 'mono', 'normal'
