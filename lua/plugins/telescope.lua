86e1c31453e4bf799ca7d38e4749cbcc583ab96e
Rentib <sbitner420@tutanota.com>
2024-11-05 08:16:49 +0100

    refactor(telescope): use defaults for fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,12 +17,7 @@ return {
             },
 
             extensions = {
-                fzf = {
-                    fuzzy = true,
-                    override_generic_sorter = true,
-                    override_file_sorter = true,
-                    case_mode = "smart_case",
-                },
+                fzf = {},
             },
         })
 
