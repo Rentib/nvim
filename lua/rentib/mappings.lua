2907a9d136b84883c53ce2a9e6b25d38c0b87e32
Rentib <sbitner420@tutanota.com>
2024-11-06 18:57:38 +0100

    feat(mappings)!: lsp mappings
    put in default lsp mappings (for older nvim versions)
    removed unimpaired mappings, as i don't use them and they're default

--- lua/rentib/mappings.lua
+++ lua/rentib/mappings.lua
@@ -10,12 +10,7 @@ m.n("<down>",  "<c-w>-")
 m.n("<up>",    "<c-w>+")
 m.n("<right>", "<c-w>>")
 
-m.n("<c-q>",     "<C-w>q")
-
-m.n("[q", "<cmd>cprevious<cr>zz")
-m.n("[Q", "<cmd>cfirst<cr>zz")
-m.n("]q", "<cmd>cnext<cr>zz")
-m.n("]Q", "<cmd>clast<cr>zz")
+m.n("<c-q>", "<C-w>q")
 
 -- smart indentation with 'i',
 m.n('i', function()
@@ -38,13 +33,18 @@ m.t("<esc>", "<c-\\><c-n>")
 -- lsp mappings
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(e)
+        -- defaults: grn - rename, grr - references, gri - implementation, g0 - document symbol, gra - code action
         local opts = { noremap = true, silent = true, buffer = e.buf }
-        m.n("<space>ca", vim.lsp.buf.code_action,     opts)
-        m.n("<space>cn", vim.lsp.buf.rename,          opts)
-        m.n("gd",        vim.lsp.buf.definition,      opts)
-        m.n("gD",        vim.lsp.buf.declaration,     opts)
-        m.n("gT",        vim.lsp.buf.type_definition, opts)
+        m.n("grn", vim.lsp.buf.rename,          opts)
+        m.n("grr", vim.lsp.buf.references,      opts)
+        m.n("gri", vim.lsp.buf.implementation,  opts)
+        m.n("gO",  vim.lsp.buf.document_symbol, opts)
+        m.n("gra", vim.lsp.buf.code_action,     opts)
 
         vim.api.nvim_create_user_command("Format", function() vim.lsp.buf.format { async = true } end, {})
     end
 })
+
+m.n("<leader>ih", function()
+    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = 0 }, { bufnr = 0 })
+end)
