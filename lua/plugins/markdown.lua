13b7008a6d879afd90e3fc61dde217c7e1ba1c51
Rentib <sbitner420@tutanota.com>
2024-11-07 10:53:38 +0100

    feat: markdown plugins

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        ft = "markdown",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons"
+        },
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end, -- on first run call by hand
+    },
+}
