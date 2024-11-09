5ec5909e2cddd2e5ec0618f72e1857853ffffe86
Rentib <sbitner420@tutanota.com>
2024-11-09 21:29:46 +0100

    feat(lazy): use retrobox during setup

--- lua/rentib/lazy.lua
+++ lua/rentib/lazy.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     spec = { { import = "plugins" } },
     defaults = { lazy = false },
-    install = { colorscheme = { "gruvbox-material", "desert" } },
+    install = { colorscheme = { "gruvbox-material", "retrobox" } },
     checker = { enabled = false },
     change_detection = { notify = false },
     performance = {
