077023c41fb0bc6c03e3bdaa44b0daaad1f0e618
Rentib <sbitner420@tutanota.com>
2024-11-09 15:18:29 +0100

    feat(cmp): back to nvim-cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,79 +1,49 @@
 return {
-    'saghen/blink.cmp',
-    lazy = false,
-    build = 'cargo build --release',
-    opts = {
-        keymap = {
-            ["<c-n>"] = { "show", "select_next", "fallback" },
-            ["<c-p>"] = { "show", "select_prev", "fallback" },
-            ["<c-e>"] = { "hide", "fallback" },
-            ["<c-y>"] = { "select_and_accept", "fallback" },
-            ["<c-space>"] = { "select_and_accept", "fallback" },
-            ["<c-j>"] = { "snippet_forward", "fallback" },
-            ["<c-k>"] = { "snippet_backward", "fallback" },
-        },
-
-        accept = {
-            create_undo_point = true,
-            auto_brackets = {
-                enabled = false,
-            },
-        },
-
-        trigger = {
-            completion = {
-                keyword_range = 'prefix',
-                keyword_regex = '[%w_\\-]',
-                exclude_from_prefix_regex = '[\\-]',
-                blocked_trigger_characters = { ' ', '\n', '\t' },
-                show_on_insert_on_trigger_character = true,
-                show_on_insert_blocked_trigger_characters = { "'", '"' },
-                show_in_snippet = false,
-            },
-
-            signature_help = {
-                enabled = true,
-                blocked_trigger_characters = {},
-                blocked_retrigger_characters = {},
-                show_on_insert_on_trigger_character = true,
-            },
-        },
-
-        fuzzy = {
-            use_frecency = true,
-            use_proximity = true,
-            max_items = 100,
-            sorts = { 'label', 'kind', 'score' },
-        },
-
-        sources = {
-            completion = {
-                enabled_providers = { 'lsp', 'path', 'snippets', 'buffer' },
-            },
-        },
-
-        windows = {
-            autocomplete = {
-                border = 'none',
-                auto_show = true,
-                selection = 'auto_insert', -- 'preselect', 'manual', 'auto_insert'
-                draw = 'minimal',          -- 'simple', 'reversed', 'minimal',
-                cycle = {
-                    from_bottom = true,
-                    from_top = true,
-                },
-            },
-            ghost_text = {
-                enabled = true,
-            },
-        },
-
-        highlight = {
-            ns = vim.api.nvim_create_namespace('blink_cmp'),
-            use_nvim_cmp_as_default = false,
-        },
-
-        nerd_font_variant = 'normal', -- 'mono', 'normal'
-        blocked_filetypes = {},
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter" },
+    dependencies = {
+        -- { "hrsh7th/cmp-nvim-lsp" },
+        { "iguanacucumber/mag-nvim-lsp",                   name = "cmp-nvim-lsp", opts = {} },
+        { "hrsh7th/cmp-nvim-lsp-signature-help" },
+        -- { "hrsh7th/cmp-buffer" },
+        { "iguanacucumber/mag-buffer",                     name = "cmp-buffer" },
+        { "https://codeberg.org/FelipeLema/cmp-async-path" },
     },
+    config = function()
+        local cmp = require("cmp")
+        local mapping = {
+            ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+            ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+            ["<C-e>"] = cmp.mapping.abort(),
+            ["<C-y>"] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Insert,
+                select = true,
+            },
+            ["<C-space>"] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Insert,
+                select = true,
+            },
+        }
+        cmp.setup({
+            performance = { debounce = 0, throttle = 0 },
+            snippet = { expand = function(args) vim.snippet.expand(args.body) end },
+            mapping = cmp.mapping.preset.insert(mapping),
+            sources = cmp.config.sources({
+                {
+                    name = "nvim_lsp",
+                    max_item_count = 5,
+                    entry_filter = function(entry) return entry:get_kind() ~= cmp.lsp.CompletionItemKind.Snippet end,
+                },
+                { name = "nvim_lsp_signature_help" },
+                {
+                    name = "buffer",
+                    max_item_count = 5,
+                    option = { keyword_pattern = [[\k\+]] },
+                },
+                { name = "async_path" }, -- "path"
+            }),
+            experimental = { ghost_text = true },
+            formatting = {},
+        })
+    end
 }
