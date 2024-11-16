fef9e3b179318ee1aeda4beff6e70708af311174
Rentib <sbitner420@tutanota.com>
2024-11-16 21:39:37 +0100

    feat(lsp): nvim-lint and ruff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -35,6 +35,9 @@ local servers = {
         filetypes = { "asm", "nasm", "masm" },
     },
 }
+
+local linters = {
+    python = { 'ruff' },
 }
 
 return {
@@ -75,5 +78,20 @@ return {
                 setup_server(server, config)
             end
         end
+    },
+    {
+        "mfussenegger/nvim-lint",
+        event = { "BufRead", "BufNewFile" },
+        config = function()
+            local lint = require("lint")
+
+            lint.linters_by_ft = linters
+
+            local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+            vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+                group = lint_augroup,
+                callback = function() lint.try_lint() end,
+            })
+        end,
     },
 }
