10527517b14acac91ada05393a1317309777e044
Rentib <sbitner420@tutanota.com>
2024-12-04 23:54:07 +0100

    feat(tpope): remove vim-commentary

--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -1,23 +1,13 @@
 return {
     { 'tpope/vim-fugitive',       cmd = { 'G' } },
-    { 'tpope/vim-commentary',     keys = { { 'gc', mode = { 'n', 'v' } } } },
+    -- { 'tpope/vim-commentary',     keys = { { 'gc', mode = { 'n', 'v' } }, { 'gcc', mode = { 'n', 'v' } } } },
+    -- { 'numToStr/Comment.nvim',    keys = { { 'gc', mode = { 'n', 'v' } }, { 'gcc', mode = { 'n', 'v' } } }, opts = {} },
     { 'tpope/vim-surround',       keys = { 'ys', 'ds', 'cs' } },
     { 'tpope/vim-rsi',            event = { "InsertEnter", "CmdLineEnter" } },
     { "farmergreg/vim-lastplace", lazy = false },
 
-    -- { 'tpope/vim-rhubarb' },
-    -- { 'tpope/vim-dispaatch' },
-    -- {
-    --     'justinmk/vim-sneak',
-    --     keys = { 's', 'S' },
-    --     config = function()
-    --         local m = require('keymap')
-    --         vim.cmd([[
-    --           let g:sneak#label = 1
-    --           let g:sneak#use_ic_scs = 1
-    --           let g:sneak#absolute_dir = 1
-    --         ]])
-    --         m.n('s', '<Plug>Sneak_s', { noremap = false })
-    --     end
-    -- },
+    -- https://github.com/tpope/vim-endwise
+    -- https://github.com/tpope/vim-rhubarb
+    -- https://github.com/tpope/vim-abolish
+    -- https://github.com/tpope/vim-eunuch
 }
