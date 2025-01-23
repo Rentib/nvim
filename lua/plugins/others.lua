77e438720d21c43a2a7b240c2d559c95851fb697
Rentib <sbitner420@tutanota.com>
2025-01-23 12:23:53 +0100

    feat: remove unused plugins

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -2,41 +2,25 @@ return {
     {
         "Rentib/cliff.nvim",
         keys = {
-            { '<c-j>', mode = { 'n', 'v', 'o' }, function() require("cliff").go_down() end },
-            { '<c-k>', mode = { 'n', 'v', 'o' }, function() require("cliff").go_up()   end },
+            { "<c-j>", mode = { "n", "v", "o" }, function() require("cliff").go_down() end },
+            { "<c-k>", mode = { "n", "v", "o" }, function() require("cliff").go_up() end },
         },
     },
 
-    { 'github/copilot.vim', cmd = 'Copilot' },
+    { "github/copilot.vim", cmd = "Copilot" },
+
+    { "lervag/vimtex",      ft = "tex" },
 
     {
-        'brenoprata10/nvim-highlight-colors',
-        cmd = { 'HighlightColors' },
-        ft = { 'css' },
-        opts = {
-            render = 'background',
-            enable_named_colors = true,
-            enable_tailwind = false,
-        },
-    },
-
-    { "chrisbra/csv.vim", ft = "csv" },
-
-    { 'lervag/vimtex', ft = 'tex' },
-
-    {
-        'akinsho/flutter-tools.nvim',
-        ft = 'dart',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        "akinsho/flutter-tools.nvim",
+        ft = "dart",
+        dependencies = { "nvim-lua/plenary.nvim" },
         opts = {},
     },
 
     {
-        'mrcjkb/rustaceanvim',
-        version = '^5', -- Recommended
-        lazy = false, -- This plugin is already lazy
+        "mrcjkb/rustaceanvim",
+        version = "^5",
+        lazy = false,
     },
-
-    -- TODO: see it these plugins are cool
-    -- { 'Wansmer/treesj' },
 }
