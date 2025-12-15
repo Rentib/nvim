bd679e9b6b756bdd0a8045d5b1c038d41626d749
Rentib <sbitner420@tutanota.com>
2025-12-15 10:01:56 +0100

    refactor(options): don't bind vim.o to o and so on

--- lua/rentib/options.lua
+++ lua/rentib/options.lua
@@ -1,90 +1,89 @@
-local g, o, opt        = vim.g, vim.o, vim.opt
+vim.g.vimsyn_embed         = "l"  -- highlight lua in vimscript
+vim.g.c_syntax_for_h       = true -- .h files are c not cpp
 
-g.vimsyn_embed         = "l"  -- highlight lua in vimscript
-g.c_syntax_for_h       = true -- .h files are c not cpp
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
 
-g.loaded_node_provider = 0
-g.loaded_perl_provider = 0
-g.loaded_ruby_provider = 0
+vim.opt.filetype.plugin    = true
+vim.opt.filetype.indent    = true
 
-opt.filetype.plugin    = true
-opt.filetype.indent    = true
+vim.o.fileencoding         = "utf-8"
+vim.o.spelllang            = "pl,en"
+vim.o.spelloptions         = "camel"
 
-o.fileencoding         = "utf-8"
-o.spelllang            = "pl,en"
-o.spelloptions         = "camel"
+vim.opt.backupdir:remove(".")
+vim.o.backup     = true
+vim.o.swapfile   = false
+vim.o.undofile   = true
+vim.o.autoread   = true
 
-opt.backupdir:remove(".")
-o.backup     = true
-o.swapfile   = false
-o.undofile   = true
-o.autoread   = true
+vim.o.backspace  = "indent,eol,start"
+vim.o.confirm    = true
 
-o.backspace  = "indent,eol,start"
-o.confirm    = true
+vim.o.updatetime = 250
 
-o.updatetime = 250
+vim.opt.completeopt:append({ "menuone", "preview", "popup", "fuzzy", "noinsert", "noselect" })
+vim.o.wildmenu = true
+vim.opt.wildignore:append({ "*.o", "__pycache__", "*.pyc", "*pycache", "*.jpg", "*.png", "*.gif", "*.pdf" })
+vim.o.wildignorecase = true
+vim.o.wildmode       = "longest:full"
+vim.o.wildoptions    = "pum"
 
-opt.completeopt:append({ "menuone", "preview", "popup", "fuzzy", "noinsert", "noselect" })
-o.wildmenu = true
-opt.wildignore:append({ "*.o", "__pycache__", "*.pyc", "*pycache", "*.jpg", "*.png", "*.gif", "*.pdf" })
-o.wildignorecase = true
-o.wildmode       = "longest:full"
-o.wildoptions    = "pum"
+vim.o.laststatus     = 0
+vim.o.showtabline    = 1
+vim.o.tabline        = ""
+vim.o.ruler          = true
+vim.o.showmode       = true
+vim.o.showcmd        = true
+vim.o.display        = "lastline,truncate"
 
-o.laststatus     = 0
-o.showtabline    = 1
-o.tabline        = ""
-o.ruler          = true
-o.showmode       = true
-o.showcmd        = true
-o.display        = "lastline,truncate"
+vim.o.number         = true
+vim.o.relativenumber = true
+vim.o.numberwidth    = 2
+vim.o.signcolumn     = "yes"
+vim.o.cursorline     = true
+vim.o.cursorlineopt  = "number"
+vim.o.colorcolumn    = "+1"
 
-o.number         = true
-o.relativenumber = true
-o.numberwidth    = 2
-o.signcolumn     = "yes"
-o.cursorline     = true
-o.cursorlineopt  = "number"
-o.colorcolumn    = "+1"
+vim.o.incsearch      = true
+vim.o.hlsearch       = true
+vim.o.ignorecase     = true
+vim.o.smartcase      = true
+vim.o.inccommand     = "split"
 
-o.incsearch      = true
-o.hlsearch       = true
-o.ignorecase     = true
-o.smartcase      = true
-o.inccommand     = "split"
+vim.o.autoindent     = true
+vim.o.smartindent    = true
+vim.o.cindent        = true
+vim.o.shiftwidth     = 4
+vim.o.tabstop        = 4
+vim.o.softtabstop    = 4
+vim.o.expandtab      = true
+vim.o.shiftround     = true
 
-o.autoindent     = true
-o.smartindent    = true
-o.cindent        = true
-o.shiftwidth     = 4
-o.tabstop        = 4
-o.softtabstop    = 4
-o.expandtab      = true
-o.shiftround     = true
+vim.o.clipboard      = "unnamedplus"
 
-o.clipboard      = "unnamedplus"
+vim.o.mouse          = "nvi"
+vim.o.mousemoveevent = false
+vim.o.mousemodel     = "popup"
 
-o.mouse          = "nvi"
-o.mousemoveevent = false
-o.mousemodel     = "popup"
+vim.opt.shortmess:append("c")
 
-opt.shortmess:append("c")
-
-o.textwidth = 79
-o.linebreak = true
-o.wrap      = false
+vim.o.textwidth = 79
+vim.o.linebreak = true
+vim.o.wrap      = false
 
 vim.cmd [[ au FileType * setlocal formatoptions=crqn1]jp ]] -- https://stackoverflow.com/questions/28375119/how-to-override-options-set-by-ftplugins-in-vim
 -- o.formatoptions = o.formatoptions + "crqn1]jp" - "to/wa2vblmMB"
 
-o.sidescroll    = 1
-o.scrolloff     = 7
-o.sidescrolloff = 8
+vim.o.sidescroll    = 1
+vim.o.scrolloff     = 7
+vim.o.sidescrolloff = 8
 
-o.virtualedit   = "block"
-o.magic         = true
-o.foldmethod    = "marker"
-o.nrformats     = "bin,hex"
+vim.o.virtualedit   = "block"
+vim.o.magic         = true
+vim.o.foldmethod    = "marker"
+vim.cmd [[ au FileType * setlocal foldmethod=marker ]]
+vim.o.nrformats     = "bin,hex"
 
-o.termguicolors = true
+vim.o.termguicolors = true
