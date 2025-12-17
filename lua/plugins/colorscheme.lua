7d1e9e5d4240ae37bcd03432909389b26ab053b4
Rentib <sbitner420@tutanota.com>
2025-12-17 21:29:58 +0100

    feat(colorscheme): remove unused rosepine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,23 +1,11 @@
 return {
-    {
-        "sainnhe/gruvbox-material",
-        priority = 1000,
-        config = function()
-            vim.g.gruvbox_material_transparent_background = 2
-            vim.g.gruvbox_material_dim_inactive_windows = 1
-            vim.g.gruvbox_material_spell_foreground = "colored"
-            vim.g.gruvbox_material_better_performance = 1
-            vim.cmd.colorscheme("gruvbox-material")
-        end
-    },
-
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        priority = 1000,
-        enabled = false,
-        config = function()
-            vim.cmd("colorscheme rose-pine")
-        end
-    },
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+    config = function()
+        vim.g.gruvbox_material_transparent_background = 2
+        vim.g.gruvbox_material_dim_inactive_windows = 1
+        vim.g.gruvbox_material_spell_foreground = "colored"
+        vim.g.gruvbox_material_better_performance = 1
+        vim.cmd.colorscheme("gruvbox-material")
+    end
 }
