ee0f1b0e92157ffb4b49b66f5786ec90b723d9fb
Rentib <sbitner420@tutanota.com>
2025-12-18 19:10:11 +0100

    feat(treesitter): use main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,43 +1,80 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+    branch = "main",
     config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                -- "c",
-                -- "lua", "luadoc", "vim", "vimdoc", "query",
-                --
-                -- "bash", "python",
-                -- "markdown", "markdown_inline",
-                -- "make",
-                -- "cpp",
-                -- "dart",
-                -- "html", "htmldjango", "css",
-                -- "haskell", "haskell_persistent",
-                --
-                -- "git_config", "gitcommit", "passwd", "ssh_config",
-                -- "zathurarc",
-            },
-            sync_install = false, -- synchronous install of parsers
-            auto_install = false, -- install missing parsers when entering a buffer
-            ignore_install = { "javascript" },
+        -- https://github.com/vanaigr/neovim-config/blob/main/after/plugin/treesitter.lua
+        require('nvim-treesitter').setup({ install_dir = vim.fn.stdpath("data") .. '/parsers' })
 
-            highlight = {
-                enable = true,
-                disable = function(lang, buf)
-                    if lang == "tex" then return true end
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
+        local parsers = require('nvim-treesitter.parsers')
+        local config = require('nvim-treesitter.config')
+        local install = require('nvim-treesitter.install')
+        local async = require('nvim-treesitter.async')
+
+        local pcall = copcall or pcall
+
+        local function getLoadingFiletype(buf)
+            local ok, value = pcall(vim.api.nvim_buf_get_var, buf, 'my_loading_filetype')
+            if not ok then
+                value = 0
+            end
+            return value
+        end
+
+        local installing = nil
+
+        ---@async
+        local function loadParser(buf)
+            local value = getLoadingFiletype(buf)
+            value = value + 1
+            vim.api.nvim_buf_set_var(buf, 'my_loading_filetype', value)
+
+            local filetype = vim.api.nvim_get_option_value('ft', { buf = buf })
+            if filetype == 'typescriptreact' then
+                filetype = 'tsx'
+            end
+
+            local parserInfo = parsers[filetype]
+            if parserInfo == nil then
+                return
+            end
+
+            local installed = config.get_installed('parsers')
+            local found = false
+            for _, name in ipairs(installed) do
+                if name == filetype then
+                    found = true
+                    break
+                end
+            end
+            if not found then
+                if installing then -- bug: it locks neovim if installing the same language twice
+                    pcall(async.await, installing)
+                end
+
+                installing = install.install({ filetype }, { summary = true })
+                if not async.await(installing) then
+                    return
+                end
+            end
+
+            if getLoadingFiletype(buf) ~= value then
+                return
+            end
+
+            vim.treesitter.start(buf, filetype)
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function(args)
+                async.arun(function()
+                    local ok, err = pcall(loadParser, args.buf)
+                    if not ok then
+                        vim.notify('Error loading parser: ' .. err, vim.log.levels.ERROR)
                     end
-                end,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = false }, -- makes indenting super slow
-            textobjects = { enable = false },
+                end)
+            end,
         })
     end,
 }
